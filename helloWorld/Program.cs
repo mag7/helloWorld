53a1fcae86e134e490780aaa30a123fb97bbc002
Beth <beth.bodily@gmail.com>
2013-08-14 10:40:42 -0400

    added name functionality

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -11,6 +11,9 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Enter your name");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello" + input);
             Console.ReadKey();
         }
     }
