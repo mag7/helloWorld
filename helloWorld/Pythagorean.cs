07d10200e4db55782d88104897b60275eb19e443
Danny Fesperman <dannyfesperman@hotmail.com>
2013-08-14 13:20:13 -0400

    Created Pythagorean class

---
+++ helloWorld/Pythagorean.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    class Pythagorean
+    {
+        public decimal Side1 { get; set; }
+        public decimal Side2 { get; set; }
+        public decimal Hypotenuse { get; set; }
+
+        public Pythagorean(decimal a, decimal b)
+        {
+            Side1 = a;
+            Side2 = b;
+        }
+    }
+}
